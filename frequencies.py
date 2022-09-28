351f6713a95cfaeef9be2cefd1be1a6ce3ff7e50
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-09-28 21:48:54 +0100

    Attempt 1 at implementing frequencies method

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if item in frequencies.keys():
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+    print(frequencies)
     return frequencies
