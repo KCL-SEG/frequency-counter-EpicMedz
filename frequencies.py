f8099b9bb81f46035ff80259730c46844750f402
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-09-28 21:56:54 +0100

    Attempt 2 of implementation of frequencies function

--- frequencies.py
+++ frequencies.py
@@ -5,9 +5,9 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
-        if item in frequencies.keys():
-            frequencies[item] += 1
+        if f"{item}" in frequencies.keys():
+            frequencies[f"{item}"] += 1
         else:
-            frequencies[item] = 1
+            frequencies[f"{item}"] = 1
     print(frequencies)
     return frequencies
